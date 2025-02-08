21a5ab10a3c6dbcf3536f1c67cf1ef1a86d1de40
Vijayalaxmi Kotagi <Vijayalaxmikotagi1969@gmail.com>
2025-02-08 19:59:51 +0530

    Add files via upload

---
+++ utils/footer.php
@@ -0,0 +1,8 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <h6 style="text-align:center;"> Jain College of Engineering and Technology Hubballi
+        <P style="text-align: center;">404/4/5,Hebballi Road, Sai Nagar,Unkal,Hubballi-5800032
+    </h6>
+    </div>
+</footer>
